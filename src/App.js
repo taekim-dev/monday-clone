aa1f3856b214bb4fc58d485cd3fd0fe271ae6cf1
taewankim <taekim.dev@gmail.com>
2023-01-26 05:33:24 +0800

    added sample data into dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Dashboard from './pages/Dashboard'
+import TicketPage from "./pages/TicketPage"
+import Nav from './componenets/Nav'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Nav/>
+                <Routes>
+                    <Route path='/' element={<Dashboard/>}></Route>
+                    <Route path='/ticket' element={<TicketPage/>}></Route>
+                    <Route path='/ticket/:id' element={<TicketPage editMode={true}/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
-export default App;
+export default App
